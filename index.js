2ba23aabac0ba1e9292e2bc8bd7901a42131e2bb
vincent <vincent.arteaga@gmail.com>
2021-05-04 10:23:35 -0700

    commit tree 11608a0200be5a11e5da063025b5559053aba390

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fi = (function() {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
-
     each: function() {
+
 
     },
 
@@ -13,6 +13,50 @@ const fi = (function() {
     },
 
     reduce: function() {
+
+    },
+
+    find: function() {
+
+    },
+
+    filter: function () {
+
+    },
+
+    size: function () {
+
+    },
+
+    first: function () {
+
+    },
+
+    last: function () {
+
+    },
+
+    compact: function() {
+
+    },
+
+    sortBy: function () {
+
+    },
+
+    flatten: function() {
+
+    },
+
+    uniq: function() {
+
+    },
+
+    keys: function () {
+
+    },
+
+    values: function() {
 
     },
 
